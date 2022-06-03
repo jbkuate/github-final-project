42c45bf587632847dd08dcd29b0da335ba1f16bb
Bosco KUATE <97669030+jbkuate@users.noreply.github.com>
2022-06-03 16:55:57 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -23,6 +23,7 @@
    echo "Enter time period in years:"
    read t
 
-   s=`expr $p \* $t \* $r / 100`
+   #s=`expr $p \* $t \* $r / 100`
+   s=(($p*$t*$r/100))
    echo "The simple interest is: "
    echo $s
